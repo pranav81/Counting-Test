b2b008fdbc19295fc718b489b2200e1abb3491f9
Pranav Sridhar K <pranavksridhar@gmail.com>
2020-04-24 22:49:11 +0530

    Comments added

--- script.js
+++ script.js
@@ -1,60 +1,63 @@
 var boxes=document.querySelectorAll('td');
-var numbers=[];
+var numbers=[];                                 //Array to store the numbers 1-20
 
 for(var i=1; i<=20; i++){
   numbers.push(i);
 }
 
-var trialNo=0
+var trialNo=0                     //Used to set the value of BEST after the first trial automatically
+                                  //For subsequent trials, BEST will be set if TIME is lower than BEST
 
-action();
+action();                         //Function to be run everytime the user wants to play
+                                  //This call initiates the game for the first time
+                                  //The user can use the RESTART button to play again
 
 function action(){
 
-  for(var i=numbers.length-1; i>0; i--) {
+  for(var i=numbers.length-1; i>0; i--) {                   //Loop to shuffle the contents of numbers[]
     var j = Math.floor(Math.random()*(i + 1));
     [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
   }
 
   var i=0;
 
-  boxes.forEach(function(box){
-    box.innerHTML=numbers[i];
+  boxes.forEach(function(box){                      //Contents of numbers[] are set to boxes in the grid
+    box.innerHTML=numbers[i];                       //The boxes now contain 1-20 in random order
     i++;
   });
 
   var grid=document.querySelector('table');
-  var n=1;
+  var n=1;                                          //n is just a counting variable
 
   grid.addEventListener('click',function(e){
-    if (e.target.innerHTML==n && e.target.innerHTML<=20){
-      e.target.innerHTML=n+20;
+    if (e.target.innerHTML==n && e.target.innerHTML<=20){       //If number inside the box is <=20,
+      e.target.innerHTML=n+20;                                  //increase the value by 20
       n++;
-    }else if(e.target.innerHTML==n && e.target.innerHTML>20){
-      e.target.innerHTML=' ';
+    }else if(e.target.innerHTML==n && e.target.innerHTML>20){   //If number inside the box is >20
+      e.target.innerHTML=' ';                                   //Change the content to nothing
       n++;
     }else {
       ;
-    }
+    }                                                           // By the end of this loop, n=41
   });
 
-  grid.style.display='none';
+  grid.style.display='none';                                //Hiding the table initially to show countdown
   var countdownNumber=document.querySelector('.countd');
   var timeInSeconds=document.querySelector('.seconds');
-  var bestInSeconds=document.querySelector('.best-seconds');
+  var bestInSeconds=document.querySelector('.best-seconds');    //Declaring all elements of HTML to be used
   var restartMsg=document.querySelector('.restart-msg');
   var finalTime=document.querySelector('.final-time');
   var restartBtn=document.querySelector('.restart-btn');
-  var count=2;
+  var count=2;                                              //count is used for the initial countdown
 
-  countdownNumber.style.display='block';
+  countdownNumber.style.display='block';              //Displaying the countdown (3..2..1..)
   restartMsg.style.display='none';
-  finalTime.style.display='none';
+  finalTime.style.display='none';            //Hiding the game results, to be displayed after the game ends
   restartBtn.style.display='none';
 
-  countdownNumber.innerHTML=3;
+  countdownNumber.innerHTML=3;               //Countdown starts at 3
 
-  var countdown=setInterval(function(){
+  var countdown=setInterval(function(){      //Every second, we reduce count by 1 until it is 0
     if (count===0){
       clearInterval(countdown);
       end();
@@ -64,7 +67,7 @@ function action(){
     }
   },1000);
 
-  function end(){
+  function end(){                            //Once countdown ends, hide the countdown and display the table
     countdownNumber.style.display='none';
     restartMsg.style.display='none';
     finalTime.style.display='none';
@@ -72,36 +75,36 @@ function action(){
     grid.style.display='table';
   }
 
-  var p=0;
+  var timevar=performance.now();            //To check the time when the game starts
 
-  var timevar=performance.now();
+  setTimeout(function(){                    //Executing after 3 seconds to account for countdown
 
-  setTimeout(function(){
-    var timerset=setInterval(function(){
-      if (n<41){
-        var variable=performance.now()-3000-timevar;
+    var timerset=setInterval(function(){    //Executing every 10 ms to change the timer every 10 ms
+
+      if (n<41){                                            //To check if the game is still running
+        var variable=performance.now()-3000-timevar;        //To set the relative time using timevar
         timeInSeconds.innerHTML=(variable/1000).toFixed(2);
       }else{
-       result=timeInSeconds.innerHTML;
+       result=timeInSeconds.innerHTML;                    //If the game is over, set the result
         if (trialNo==1){
           bestInSeconds.innerHTML=result;
         }
         else{
-          if (bestInSeconds.innerHTML>result){
+          if (bestInSeconds.innerHTML>result){          //If TIME<BEST, change BEST
             bestInSeconds.innerHTML=result;
           }
         }
 
         finalTime.innerHTML=result;
 
-        clearInterval(timerset);
+        clearInterval(timerset);                 //Once game is over, clear the interval set for the time
 
         grid.style.display='none';
-        restartMsg.style.display='block';
+        restartMsg.style.display='block';               //Display the game results, hide the grid
         finalTime.style.display='block';
         restartBtn.style.display='block';
 
-        restartBtn.addEventListener('click',action);
+        restartBtn.addEventListener('click',action);    //If the RESTART button is clicked, execute action()
       }
     },10);
   },3000);
